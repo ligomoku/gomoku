42b9fdbeec6d03f7d9c0ab601871d65ce2420563
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2024-09-17 00:49:55 +0300

    implement game session handler tests

--- GomokuServer/tests/GomokuServer.Application.UnitTests/GlobalUsings.cs
+++ GomokuServer/tests/GomokuServer.Application.UnitTests/GlobalUsings.cs
@@ -1,1 +1,10 @@
+global using Ardalis.Result;
+
+global using FluentAssertions;
+
+global using GomokuServer.Application.Interfaces;
+global using GomokuServer.Core.Entities;
+
+global using NSubstitute;
+
 global using NUnit.Framework;
