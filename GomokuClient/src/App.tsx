277c3528c4b9ed5e12bf0db288a040a08bdc03e2
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-16 23:12:49 +0200

    feat: add clerk

--- GomokuClient/src/App.tsx
+++ GomokuClient/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.scss";
 import { client } from "./api/client";
-import Square from "./components/Square/Square.tsx";
+import Square from "./components/Square/Square";
 import { CellValue, useBoard } from "./hooks/useBoardLocal";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 client.setConfig({ baseUrl: import.meta.env.VITE_API_URL });
 
@@ -9,32 +15,46 @@ const App = () => {
   const { board, winner, handlePieceClick, playAgain } = useBoard();
 
   return (
-    <div className="container">
-      <div className="info">
-        <div className="title">Gomoku</div>
-        {winner && <div className="message">The Winner is: {winner}!</div>}
-        <button className="button" onClick={playAgain}>
-          Play again
-        </button>
-      </div>
-      <div className="wrapper">
-        <div className="board">
-          {board.map((row, rowIndex) => (
-            <div className="row" key={rowIndex}>
-              {row.map((_col: CellValue, colIndex: number) => (
-                <Square
-                  key={colIndex}
-                  value={board[rowIndex][colIndex]}
-                  row={rowIndex}
-                  col={colIndex}
-                  onClick={handlePieceClick}
-                />
-              ))}
-            </div>
-          ))}
+    <>
+      <div className="container">
+        <div className="info">
+          <div className="title">Gomoku</div>
+          {winner && <div className="message">The Winner is: {winner}!</div>}
+          <SignedOut>
+            <SignInButton>
+              <button className="button">Sign in</button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <button
+            className="button"
+            onClick={playAgain}
+            style={{ marginLeft: 20 }}
+          >
+            Play again
+          </button>
+        </div>
+        <div className="wrapper">
+          <div className="board">
+            {board.map((row, rowIndex) => (
+              <div className="row" key={rowIndex}>
+                {row.map((_col: CellValue, colIndex: number) => (
+                  <Square
+                    key={colIndex}
+                    value={board[rowIndex][colIndex]}
+                    row={rowIndex}
+                    col={colIndex}
+                    onClick={handlePieceClick}
+                  />
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
