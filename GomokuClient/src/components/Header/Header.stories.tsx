5ca9cd8dfd45c3a9535ea1eb68630f2d4692187e
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-17 03:33:25 +0200

    chore: more responsive header for mobile

--- GomokuClient/src/components/Header/Header.stories.tsx
+++ GomokuClient/src/components/Header/Header.stories.tsx
@@ -1,8 +1,20 @@
 import { Header } from "./Header";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 export default {
   title: "Components/Header",
   component: Header,
+  decorators: [
+    (Story: never) => (
+      <ClerkProvider
+        publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+      >
+        {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+        {/* @ts-expect-error*/}
+        <Story />
+      </ClerkProvider>
+    ),
+  ],
 };
 
 export const Default = () => <Header />;
