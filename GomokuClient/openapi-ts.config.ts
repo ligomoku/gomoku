c4158d28ca1c86fc2ea86b50912884c7de305c10
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-17 14:41:48 +0200

    feat: basic multiplayer integration

--- GomokuClient/openapi-ts.config.ts
+++ GomokuClient/openapi-ts.config.ts
@@ -3,8 +3,13 @@ import { defineConfig } from "@hey-api/openapi-ts";
 export default defineConfig({
   client: "@hey-api/client-fetch",
   input: "src/api/schema.json",
-  output: "src/api/client",
+  output: {
+    path: "src/api/client",
+    format: "prettier",
+    lint: "eslint",
+  },
   types: {
     enums: "javascript",
   },
+  plugins: ["@tanstack/react-query"],
 });
