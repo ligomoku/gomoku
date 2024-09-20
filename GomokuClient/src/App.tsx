ee5dcbdbd6007eca3d144ad5ac687c14275a693d
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-20 20:57:10 +0200

    chore: convert to tailwind app.tsx

--- GomokuClient/src/App.tsx
+++ GomokuClient/src/App.tsx
@@ -1,9 +1,8 @@
-import styles from "./App.module.scss";
 import { client } from "./api/client";
 import Square from "./components/Square/Square";
 import { CellValue, useBoard } from "./hooks/useBoardLocal";
 import { useSignalR } from "./hooks/useSignlarR.ts";
-import { Header } from "./components/Header";
+import { Header } from "@/components/Header";
 
 client.setConfig({ baseUrl: import.meta.env.VITE_API_URL });
 
@@ -18,19 +17,22 @@ const App = () => {
   return (
     <>
       <Header />
-      <div className={styles.container}>
-        <div className={styles.info}>
+      <div className="font-open-sans font-light p-4 flex flex-col items-center">
+        <div className="text-center mb-5">
           {winner && (
-            <div className={styles.message}>The Winner is: {winner}!</div>
+            <div className="text-2xl mb-2">The Winner is: {winner}!</div>
           )}
-          <button className={styles.button} onClick={playAgain}>
+          <button
+            className="text-lg font-open-sans cursor-pointer my-5 mx-auto rounded bg-gray-300 text-gray-700 transition duration-300 p-2 border border-gray-300 hover:bg-gray-200"
+            onClick={playAgain}
+          >
             Play again
           </button>
         </div>
-        <div className={styles.wrapper}>
-          <div className={styles.board}>
+        <div className="flex justify-center flex-wrap w-full mb-5">
+          <div className="flex flex-col w-full max-w-md mx-auto">
             {board.map((row, rowIndex) => (
-              <div className={styles.row} key={rowIndex}>
+              <div className="flex justify-center" key={rowIndex}>
                 {row.map((_col: CellValue, colIndex: number) => (
                   <Square
                     key={colIndex}
@@ -44,9 +46,12 @@ const App = () => {
             ))}
           </div>
         </div>
-        <div className={styles.messages}>
+        <div className="mt-5 border border-gray-300 p-4 w-full max-w-xl text-center">
           {isConnected ? (
-            <button className={styles.button} onClick={handleSendMessage}>
+            <button
+              className="text-lg font-open-sans cursor-pointer my-5 mx-auto rounded bg-gray-300 text-gray-700 transition duration-300 p-2 border border-gray-300 hover:bg-gray-200"
+              onClick={handleSendMessage}
+            >
               Send Test Message
             </button>
           ) : (
--- GomokuClient/src/main.tsx
+++ GomokuClient/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./styles/index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
