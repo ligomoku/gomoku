8a6849708ca2c2cc099b73217ba54470e5e24026
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-21 03:25:43 +0200

    chore: add tanstack with eslint tanstack

--- GomokuClient/eslint.config.js
+++ GomokuClient/eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -16,6 +17,7 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "@tanstack/query": pluginQuery,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
--- GomokuClient/openapi-ts.config.ts
+++ GomokuClient/openapi-ts.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   client: "@hey-api/client-fetch",
   input: "src/api/schema.json",
   output: "src/api/client",
+  plugins: ["@tanstack/react-query"],
   types: {
     enums: "javascript",
   },
