feb0eb5fa5202a61b6bffd71434741586552e378
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2024-09-21 22:39:18 +0300

    fix(client): routing file

--- GomokuClient/src/routes/__root.tsx
+++ GomokuClient/src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Header } from "@/shared/ui/Header";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const Route = createRootRoute({
   component: () => (
@@ -8,6 +9,7 @@ export const Route = createRootRoute({
       <Header />
       <Outlet />
       <TanStackRouterDevtools />
+      <ReactQueryDevtools initialIsOpen={false} />
     </>
   ),
 });
