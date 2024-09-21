a78dae05ff106931561ed4ec485b8db997f85a89
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-21 02:31:09 +0200

    chore: skeleton setup for locales

--- GomokuClient/vite.config.ts
+++ GomokuClient/vite.config.ts
@@ -5,16 +5,20 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact()],
+  plugins: [
+    TanStackRouterVite(),
+    viteReact({
+      babel: {
+        plugins: ["macros"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
-    hmr: {
-      overlay: false, // Disables the HMR overlay, which may fix HMR-related conflicts
-    },
     port: 4200,
   },
 });
