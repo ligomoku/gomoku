b20206312db42e9bd4a390fde1352f64f34ecfb7
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2024-09-21 22:46:54 +0300

    fix(server): api project usings

--- GomokuServer/src/GomokuServer.Api/Program.cs
+++ GomokuServer/src/GomokuServer.Api/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddSwaggerGen(options =>
 	var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
 	var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 	options.IncludeXmlComments(xmlPath);
+
 	options.ExampleFilters();
 });
 
