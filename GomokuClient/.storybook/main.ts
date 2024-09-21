95e9ef110bb05076302273102e6d993e53bd6fda
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-21 02:32:00 +0200

    chore: fix prettier

--- GomokuClient/.storybook/main.ts
+++ GomokuClient/.storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-styling-webpack"
+    "@storybook/addon-styling-webpack",
   ],
   framework: {
     name: "@storybook/react-vite",
