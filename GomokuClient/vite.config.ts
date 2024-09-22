ec6d57e8f0b092750b96baa081800cbb41b2c9f2
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-22 16:38:27 +0200

    chore: use .envs from client to vite port

--- GomokuClient/vite.config.ts
+++ GomokuClient/vite.config.ts
@@ -1,25 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    viteReact({
-      babel: {
-        plugins: ["macros"],
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, path.resolve(__dirname, "../envs"));
+
+  return defineConfig({
+    plugins: [
+      TanStackRouterVite(),
+      viteReact({
+        babel: {
+          plugins: ["macros"],
+        },
+      }),
+    ],
+    envDir: path.resolve(__dirname, "../envs"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-    }),
-  ],
-  envDir: path.resolve(__dirname, "../envs"),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    port: 4200,
-  },
-});
+    server: {
+      port: parseInt(env.VITE_LOCALHOST_PORT ?? "4500"),
+    },
+  });
+};
