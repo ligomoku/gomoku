be11de7b1fa944f142e2cdb61d5d96e7fe08230e
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-23 03:35:48 +0200

    chore: cleanups

--- GomokuServer/src/GomokuServer.Api/Program.cs
+++ GomokuServer/src/GomokuServer.Api/Program.cs
@@ -1,19 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure environment variables
 builder.Configuration.AddEnvironmentVariables();
 
-// Load environment
 var environmentLoader = new EnvironmentLoader(builder.Configuration);
 environmentLoader.LoadEnvironment();
 
-// Configure services
 var serviceConfigurator = new ServiceConfigurator();
 serviceConfigurator.ConfigureServices(builder);
 
-// Build the app
 var app = builder.Build();
 
-// Configure the app
 var appConfigurator = new AppConfigurator();
 appConfigurator.ConfigureApp(app);
