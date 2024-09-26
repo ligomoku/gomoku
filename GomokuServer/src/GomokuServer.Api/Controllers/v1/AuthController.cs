19fa63dfa435f808a3bfa0636b6335114d0b0cb3
Aleksandrs Vaguscenko <54764339+Alequez97@users.noreply.github.com>
2024-09-26 21:57:17 +0300

    feat(server): jwt validation (#47)

---
+++ GomokuServer/src/GomokuServer.Api/Controllers/v1/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using GomokuServer.Api.Attributes;
+using GomokuServer.Api.Controllers.v1.Responses;
+
+namespace GomokuServer.Api.Controllers.v1;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/[controller]")]
+[EnableCors(CorsPolicyName.GomokuClient)]
+[Consumes(MediaTypeNames.Application.Json)]
+[Produces(MediaTypeNames.Application.Json)]
+[ClerkAuthorization]
+public class AuthController : Controller
+{
+	/// <summary>
+	/// Get auth user information
+	/// </summary>
+	[HttpGet("info")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	public async Task<IActionResult> AuthInfo()
+	{
+		var usernameClaim = User.Claims.FirstOrDefault(c => c.Type == "username")?.Value;
+		var fullNameClaim = User.Claims.FirstOrDefault(c => c.Type == "fullName")?.Value;
+
+		var response = new AuthInfoResponse()
+		{
+			UserName = usernameClaim,
+			FullName = fullNameClaim,
+		};
+
+		return Ok(response);
+	}
+}
---
+++ GomokuServer/src/GomokuServer.Api/Controllers/v1/Responses/AuthInfoResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace GomokuServer.Api.Controllers.v1.Responses;
+
+public class AuthInfoResponse
+{
+	public string? UserName { get; set; }
+	public string? FullName { get; set; }
+}
