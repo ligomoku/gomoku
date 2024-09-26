148956bd7e8832a27de60ad9cfa16e609f9f2d53
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2024-09-26 21:25:23 +0300

    Initial implementation

---
+++ GomokuServer/src/GomokuServer.Api/Controllers/v1/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using GomokuServer.Api.Attributes;
+
+namespace GomokuServer.Api.Controllers.v1;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/[controller]")]
+[EnableCors(CorsPolicyName.GomokuClient)]
+[Consumes(MediaTypeNames.Application.Json)]
+[Produces(MediaTypeNames.Application.Json)]
+[ClerkAuthorization]
+public class AuthController : Controller
+{
+	/// <summary>
+	/// Get auth user information
+	/// </summary>
+	[HttpGet("info")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	public async Task<IActionResult> AuthInfo()
+	{
+		return Ok();
+	}
+}
--- GomokuServer/src/GomokuServer.Api/Middlewares/ClerkJwtValidationMiddleware.cs
+++ GomokuServer/src/GomokuServer.Api/Middlewares/ClerkJwtValidationMiddleware.cs
@@ -2,6 +2,10 @@
 
 using Microsoft.Extensions.Caching.Memory;
 
+using Refit;
+
+using ProblemDetails = Microsoft.AspNetCore.Mvc.ProblemDetails;
+
 namespace GomokuServer.Api.Middlewares;
 
 public class ClerkJwtValidationMiddleware
@@ -9,63 +13,78 @@ public class ClerkJwtValidationMiddleware
 	private readonly RequestDelegate _next;
 	private readonly IClerkFrontendApi _clerkClientApi;
 	private readonly IMemoryCache _memoryCache;
+	private readonly ILogger<ClerkJwtValidationMiddleware> _logger;
 	private const string JWKS_CACHE_KEY = "ClerkJWKS";
 
-	public ClerkJwtValidationMiddleware(RequestDelegate next, IClerkFrontendApi clerkClientApi, IMemoryCache memoryCache)
+	public ClerkJwtValidationMiddleware(RequestDelegate next, IClerkFrontendApi clerkClientApi, IMemoryCache memoryCache, ILogger<ClerkJwtValidationMiddleware> logger)
 	{
 		_next = next;
 		_clerkClientApi = clerkClientApi;
 		_memoryCache = memoryCache;
+		_logger = logger;
 	}
 
 	public async Task InvokeAsync(HttpContext context)
 	{
-		var endpoint = context.GetEndpoint();
-		var enpointMetadata = endpoint?.Metadata;
-
-		var clerkAuthorizationAttribute = enpointMetadata?.FirstOrDefault(x => x.GetType() == typeof(ClerkAuthorizationAttribute));
-
-		if (clerkAuthorizationAttribute == null)
-		{
-			await _next(context);
-			return;
-		}
-
-		var token = context.Request.Headers.Authorization.ToString().Replace("Bearer ", "");
-
-		if (string.IsNullOrWhiteSpace(token))
-		{
-			context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-			await context.Response.WriteAsJsonAsync(GetUnauthorizedResponse("Token is missing"));
-			return;
-		}
-
-		var jwksJson = await _memoryCache.GetOrCreateAsync(JWKS_CACHE_KEY, async entry =>
-		{
-			entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1);
-			return await _clerkClientApi.GetJwks();
-		});
-		var jsonWebKeySet = new JsonWebKeySet(jwksJson);
-
-		var tokenHandler = new JwtSecurityTokenHandler();
-		var validationParameters = new TokenValidationParameters
-		{
-			ValidateIssuer = false,
-			ValidateAudience = false,
-			ValidateIssuerSigningKey = true,
-			ValidateLifetime = true,
-			IssuerSigningKeys = jsonWebKeySet.GetSigningKeys()
-		};
-
 		try
 		{
-			var principal = tokenHandler.ValidateToken(token, validationParameters, out _);
-			await _next(context);
-		}
-		catch (SecurityTokenException)
+			var endpoint = context.GetEndpoint();
+			var enpointMetadata = endpoint?.Metadata;
+
+			var clerkAuthorizationAttribute = enpointMetadata?.FirstOrDefault(x => x.GetType() == typeof(ClerkAuthorizationAttribute));
+
+			if (clerkAuthorizationAttribute == null)
+			{
+				await _next(context);
+				return;
+			}
+
+			var token = context.Request.Headers.Authorization.ToString().Replace("Bearer ", "");
+
+			if (string.IsNullOrWhiteSpace(token))
+			{
+				context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+				await context.Response.WriteAsJsonAsync(GetUnauthorizedResponse("Token is missing"));
+				return;
+			}
+
+			var jwksJson = await _memoryCache.GetOrCreateAsync(JWKS_CACHE_KEY, async entry =>
+			{
+				entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1);
+				return await _clerkClientApi.GetJwks();
+			});
+			var jsonWebKeySet = new JsonWebKeySet(jwksJson);
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var validationParameters = new TokenValidationParameters
+			{
+				ValidateIssuer = false,
+				ValidateAudience = false,
+				ValidateIssuerSigningKey = true,
+				ValidateLifetime = true,
+				IssuerSigningKeys = jsonWebKeySet.GetSigningKeys()
+			};
+
+			try
+			{
+				var principal = tokenHandler.ValidateToken(token, validationParameters, out _);
+				await _next(context);
+			}
+			catch (SecurityTokenException)
+			{
+				context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+				await context.Response.WriteAsJsonAsync(GetUnauthorizedResponse("Token validation failed"));
+			}
+		} 
+		catch (ApiException exception)
 		{
-			context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-			await context.Response.WriteAsJsonAsync(GetUnauthorizedResponse("Token validation failed"));
+			_logger.LogError("Unable to fetch jwks file from clerk. Check url in configuration");
+			context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+		}
+		catch (Exception exception) 
+		{
+			_logger.LogError(exception, exception.Message);
+			context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
 		}
 	}
 
