130f2c06eb8d0c8100a90a2888a87637660552cf
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-28 01:26:48 +0200

    chore: formatted files

--- GomokuClient/packages/gomoku-core/src/routes/game/$gameID.tsx
+++ GomokuClient/packages/gomoku-core/src/routes/game/$gameID.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/game/$gameID')({
+export const Route = createFileRoute("/game/$gameID")({
   component: () => <div>Hello /game/$gameID!</div>,
-})
+});
