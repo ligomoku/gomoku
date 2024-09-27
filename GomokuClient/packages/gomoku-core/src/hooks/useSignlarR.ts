4199b25a711e14760f1d99cca7218518ea142e7d
Vitaly Kuprin <51091179+vkuprin@users.noreply.github.com>
2024-09-27 04:31:16 +0300

    Update docker-test.yml
    chore: format
    
    chore: cleanups

--- GomokuClient/packages/gomoku-core/src/hooks/useSignlarR.ts
+++ GomokuClient/packages/gomoku-core/src/hooks/useSignlarR.ts
@@ -4,17 +4,17 @@ import { MessagePackHubProtocol } from "@microsoft/signalr-protocol-msgpack";
 
 export const useSignalR = () => {
   const [connection, setConnection] = useState<signalR.HubConnection | null>(
-      null,
+    null,
   );
   const [messages, setMessages] = useState<string[]>([]);
   const [isConnected, setIsConnected] = useState<boolean>(false);
 
   useEffect(() => {
     const newConnection = new signalR.HubConnectionBuilder()
-        .withUrl(`${import.meta.env.VITE_API_URL}/gamehub`)
-        .withHubProtocol(new MessagePackHubProtocol()) // Add this line
-        .withAutomaticReconnect()
-        .build();
+      .withUrl(`${import.meta.env.VITE_API_URL}/gamehub`)
+      .withHubProtocol(new MessagePackHubProtocol())
+      .withAutomaticReconnect()
+      .build();
 
     setConnection(newConnection);
   }, []);
@@ -22,20 +22,20 @@ export const useSignalR = () => {
   useEffect(() => {
     if (connection) {
       connection
-          .start()
-          .then(() => {
-            console.log("Connected to SignalR hub");
-            setIsConnected(true);
+        .start()
+        .then(() => {
+          console.log("Connected to SignalR hub");
+          setIsConnected(true);
 
-            connection.on("ReceiveMessage", (user: string, message: string) => {
-              setMessages((messages) => [...messages, `${user}: ${message}`]);
-            });
+          connection.on("ReceiveMessage", (user: string, message: string) => {
+            setMessages((messages) => [...messages, `${user}: ${message}`]);
+          });
 
-            connection.on("GameUpdate", (data) => {
-              console.log("Game update received:", data);
-            });
-          })
-          .catch((error) => console.error("Connection failed: ", error));
+          connection.on("GameUpdate", (data) => {
+            console.log("Game update received:", data);
+          });
+        })
+        .catch((error) => console.error("Connection failed: ", error));
 
       return () => {
         connection.stop();
