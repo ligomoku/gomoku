f1468fbe3a0322d75dcc0974a9bddbfe99fb8467
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2024-09-28 02:00:37 +0300

    feat(server): add headers to swagger types

--- GomokuServer/src/GomokuServer.Api/Controllers/v1/GameController.cs
+++ GomokuServer/src/GomokuServer.Api/Controllers/v1/GameController.cs
@@ -72,6 +72,7 @@ public class GameController : Controller
 	[ProducesResponseType(StatusCodes.Status200OK)]
 	[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
 	[SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(NotFoundErrorExample))]
+	[AddCustomHeaders]
 	public async Task<IActionResult> AddPlayerToGame([FromRoute] string gameId, [FromRoute] string playerId)
 	{
 		var addPlayerToGameResult = await _gameSessionHandler.AddPlayerToGameAsync(gameId, playerId);
@@ -91,6 +92,7 @@ public class GameController : Controller
 	[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
 	[SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestErrorExample))]
 	[SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(NotFoundErrorExample))]
+	[AddCustomHeaders]
 	public async Task<IActionResult> MakeMove([FromRoute] string gameId, [FromRoute] string playerId, [FromBody] MakeMoveRequest request)
 	{
 		var placeTileResult = await _gameSessionHandler.PlaceTileAsync(gameId, new TileDto(request.X, request.Y), playerId);
