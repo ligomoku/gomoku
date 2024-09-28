d425fbef0b1b3e83f1d638428c4fbd4517f8cc18
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-28 18:08:05 +0200

    chore: more generic ui

--- GomokuClient/packages/gomoku-core/src/features/GameOptionsButton/GameOptionsButton.stories.tsx
+++ GomokuClient/packages/gomoku-core/src/features/GameOptionsButton/GameOptionsButton.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryFn } from "@storybook/react";
+import { Meta } from "@storybook/react";
 import { GameOptionsButtons } from "./GameOptionsButton";
 
 export default {
@@ -9,10 +9,12 @@ export default {
   },
 } as Meta<typeof GameOptionsButtons>;
 
-const Template: StoryFn<typeof GameOptionsButtons> = () => (
-  <GameOptionsButtons />
+export const Default = () => (
+  <GameOptionsButtons
+    onCreateGameClick={() => alert("Create Game Clicked")}
+    onPlayWithFriendClick={() => alert("Play with Friend Clicked")}
+    onPlayWithAIClick={() => alert("Play with AI Clicked")}
+  />
 );
-
-export const Default = Template.bind({});
 
 Default.args = {};
--- GomokuClient/packages/gomoku-core/src/pages/Home.tsx
+++ GomokuClient/packages/gomoku-core/src/pages/Home.tsx
@@ -3,29 +3,35 @@ import { TimeControls } from "@/features/TimeControls";
 import { OnlinePlayersInfo } from "@/features/OnlinePlayersInfo";
 import { FeaturedBoxes } from "@/features/FeaturedBoxes";
 import { GameOptionsButtons } from "@/features/GameOptionsButton";
+import { useNavigate } from "@tanstack/react-router";
 
-export const Home = () => (
-  <div className="min-h-screen bg-[#161512] text-base text-[#bababa] sm:text-lg">
-    <main className="container mx-auto p-4 sm:p-6">
-      <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
-        <div className="lg:col-span-3">
-          <Tournaments />
-          <p className="text-base sm:text-lg">
-            Gomoku.org is a free, open-source Five in a Row game platform.
-          </p>
+export const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="min-h-screen bg-[#161512] text-base text-[#bababa] sm:text-lg">
+      <main className="container mx-auto p-4 sm:p-6">
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
+          <div className="lg:col-span-3">
+            <Tournaments />
+            <p className="text-base sm:text-lg">
+              Gomoku.org is a free, open-source Five in a Row game platform.
+            </p>
+          </div>
+          <div className="lg:col-span-6">
+            <h2 className="mb-6 text-2xl font-bold text-[#bababa] sm:text-3xl">
+              Quick pairing
+            </h2>
+            <TimeControls />
+          </div>
+          <div className="lg:col-span-3">
+            <GameOptionsButtons
+              onCreateGameClick={() => navigate({ to: "/game" })}
+            />
+            <OnlinePlayersInfo />
+          </div>
         </div>
-        <div className="lg:col-span-6">
-          <h2 className="mb-6 text-2xl font-bold text-[#bababa] sm:text-3xl">
-            Quick pairing
-          </h2>
-          <TimeControls />
-        </div>
-        <div className="lg:col-span-3">
-          <GameOptionsButtons />
-          <OnlinePlayersInfo />
-        </div>
-      </div>
-      <FeaturedBoxes />
-    </main>
-  </div>
-);
+        <FeaturedBoxes />
+      </main>
+    </div>
+  );
+};
