0397cb2415ccb2813d63fd0ee5273bf5fb77c1e0
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-29 00:46:04 +0200

    fix: typecheck
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/src/shared/ui/Header/Header.tsx
+++ GomokuClient/packages/gomoku-core/src/shared/ui/Header/Header.tsx
@@ -31,7 +31,10 @@ export const Header = () => {
         <div
           className={`${isMenuOpen ? "flex" : "hidden"} absolute left-0 top-16 w-full flex-col items-start space-y-4 bg-[#2b2b2b] p-4 text-[#bababa] sm:relative sm:top-0 sm:flex sm:w-auto sm:flex-row sm:items-center sm:space-x-6 sm:space-y-0 sm:bg-transparent sm:p-0`}
         >
-          <Link to="/game" className="text-lg hover:text-[#f0f0f0] sm:text-xl">
+          <Link
+            to="/game/create"
+            className="text-lg hover:text-[#f0f0f0] sm:text-xl"
+          >
             PLAY
           </Link>
           <Link href="#" className="text-lg hover:text-[#f0f0f0] sm:text-xl">
