834db67be9125a4740eceea0d5ac4acfc5e98df9
Aleksandrs Vaguscenko <54764339+Alequez97@users.noreply.github.com>
2024-09-30 00:18:29 +0300

    feat(servers): initial make move game hub implementation (#108)
    Signed-off-by: Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>

---
+++ GomokuServer/src/GomokuServer.Api/Hubs/Messages/Client/MakeMoveClientMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace GomokuServer.Api.Hubs.Messages.Client;
+
+public record MakeMoveClientMessage
+{
+	[Required]
+	public required string GameId { get; init; }
+
+	[Required]
+	public required int X { get; init; }
+
+	[Required]
+	public required int Y { get; init; }
+}
