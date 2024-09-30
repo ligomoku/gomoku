3d0d5f20f09c7acfd5350dff1bed115eab6b3641
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-30 02:39:10 +0200

    chore: formatter
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuServer/src/GomokuServer.Api/Hubs/Messages/Client/ReceiveClientMessage.cs
+++ GomokuServer/src/GomokuServer.Api/Hubs/Messages/Client/ReceiveClientMessage.cs
@@ -5,7 +5,7 @@ using MessagePack;
 [MessagePackObject]
 public record ChatMessageClientMessage
 {
-	[Key("user")] [Required] public required string User { get; init; }
+	[Key("user")][Required] public required string User { get; init; }
 
-	[Key("message")] [Required] public required string Message { get; init; }
+	[Key("message")][Required] public required string Message { get; init; }
 }
