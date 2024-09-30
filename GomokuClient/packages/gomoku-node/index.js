389fc57668396258424098cf06b48c706a252131
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-30 03:10:52 +0200

    chore: pausing
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-node/index.js
+++ GomokuClient/packages/gomoku-node/index.js
@@ -1,12 +1,32 @@
+const signalR = require("@microsoft/signalr");
 const msgpack = require("@msgpack/msgpack");
 
 const data = {
   user: "meg",
-  message: "hello",
+  message: "hello gomoku",
 };
 
 const encoded = msgpack.encode(data);
 console.log("Encoded MessagePack:", encoded);
+
+const connection = new signalR.HubConnectionBuilder()
+  //TODO: connect to node .env file
+  .withUrl("http://localhost:62411/gamehub")
+  .build();
+
+async function startAndSendMessage() {
+  try {
+    await connection.start();
+    console.log("SignalR connection established");
+
+    await connection.send("SendMessage", encoded);
+    console.log("Message sent successfully");
+  } catch (error) {
+    console.error("Error starting connection or sending message:", error);
+  }
+}
+
+startAndSendMessage();
 
 const decoded = msgpack.decode(encoded);
 console.log("Decoded Data:", decoded);
