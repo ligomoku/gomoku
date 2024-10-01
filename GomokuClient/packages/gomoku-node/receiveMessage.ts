97136a650ecb6c00a763e6f0e38de5d1317f82c7
Vitaly Kuprin <51091179+vkuprin@users.noreply.github.com>
2024-10-01 02:19:52 +0200

    feat(client): typescript support for gomoku-node (#115)
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-node/receiveMessage.ts
+++ GomokuClient/packages/gomoku-node/receiveMessage.ts
@@ -1,4 +1,4 @@
-const signalR = require("@microsoft/signalr");
+import * as signalR from "@microsoft/signalr";
 
 const connection = new signalR.HubConnectionBuilder()
   .withUrl("http://localhost:62411/gamehub")
