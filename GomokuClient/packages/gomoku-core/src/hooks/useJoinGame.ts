ea46d46f30422a4a31491219c5a5f2505994220b
Vitaly Kuprin <vitas_s@inbox.lv>
2024-10-02 03:22:49 +0200

    chore: separation of concerns
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

---
+++ GomokuClient/packages/gomoku-core/src/hooks/useJoinGame.ts
@@ -0,0 +1,67 @@
+import { useEffect } from "react";
+import { useBoard } from "@/hooks/useBoard";
+import { typedStorage } from "@/shared/lib/utils";
+import { useSignalRConnection } from "@/context";
+import * as signalR from "@microsoft/signalr";
+
+export const useJoinGame = (gameID: string) => {
+  const { board, winner, addPiece } = useBoard(gameID);
+  const { connection, isConnected, registerEventHandlers } =
+    useSignalRConnection();
+
+  useEffect(() => {
+    const previousGameID = typedStorage.getItem("currentGameID");
+    if (previousGameID && previousGameID !== gameID) {
+      // TODO: Improve this logic by fetching if the game is available and if not, remove data
+      typedStorage.removeItem(`gameBoard_${previousGameID}`);
+      typedStorage.removeItem(`nextTurn_${previousGameID}`);
+    }
+    typedStorage.setItem("currentGameID", gameID);
+  }, [gameID]);
+
+  useEffect(() => {
+    if (isConnected && gameID) {
+      connection?.invoke("JoinGameGroup", gameID);
+
+      return registerEventHandlers({
+        onPlayerJoined: ({ userName }) => {
+          console.log(`Player ${userName} joined game.`);
+        },
+        onPlayerMadeMove: ({ playerId, tile, placedTileColor }) => {
+          console.log("Player made move:", playerId, tile, placedTileColor);
+          addPiece(tile.y, tile.x, placedTileColor);
+        },
+        onGameHubError: (error) => {
+          console.warn("Error from game hub:", error.message);
+        },
+      });
+    }
+  }, [connection, isConnected, gameID, registerEventHandlers, addPiece]);
+
+  useEffect(() => {
+    if (winner) alert(`The winner is: ${winner}`);
+  }, [winner]);
+
+  const handleMove = async (row: number, col: number, value: string | null) => {
+    if (!connection || winner || value) return;
+
+    if (connection.state !== signalR.HubConnectionState.Connected) {
+      console.warn("Cannot make a move, SignalR connection is not connected.");
+      return;
+    }
+
+    const makeMoveMessage = {
+      gameId: gameID,
+      x: row,
+      y: col,
+    };
+
+    try {
+      await connection.invoke("MakeMove", makeMoveMessage);
+    } catch (error) {
+      console.error("Error making move:", error);
+    }
+  };
+
+  return { board, winner, handleMove };
+};
--- GomokuClient/packages/gomoku-core/src/pages/JoinGame.tsx
+++ GomokuClient/packages/gomoku-core/src/pages/JoinGame.tsx
@@ -1,74 +1,12 @@
-import { useEffect } from "react";
-import Square from "@/features/Square/Square";
-import { useBoard } from "@/hooks/useBoard";
+import { useParams } from "@tanstack/react-router";
 import { Timer } from "@/features/Timer";
 import { Chat } from "@/features/Chat";
-import { useParams } from "@tanstack/react-router";
-import { typedStorage } from "@/shared/lib/utils";
-import { useSignalRConnection } from "@/context";
-import * as signalR from "@microsoft/signalr";
+import Square from "@/features/Square/Square";
+import { useJoinGame } from "@/hooks/useJoinGame";
 
 const JoinGame = () => {
   const { gameID } = useParams({ strict: false });
-
-  useEffect(() => {
-    const previousGameID = typedStorage.getItem("currentGameID");
-    if (previousGameID && previousGameID !== gameID) {
-      // TODO: Improve this logic by fetching if the game is available and if not, remove data
-      typedStorage.removeItem(`gameBoard_${previousGameID}`);
-      typedStorage.removeItem(`nextTurn_${previousGameID}`);
-    }
-    typedStorage.setItem("currentGameID", gameID!);
-  }, [gameID]);
-
-  const { board, winner, addPiece } = useBoard(gameID!);
-
-  const { connection, isConnected, registerEventHandlers } =
-    useSignalRConnection();
-
-  useEffect(() => {
-    if (isConnected && gameID) {
-      connection?.invoke("JoinGameGroup", gameID);
-
-      return registerEventHandlers({
-        onPlayerJoined: ({ userName }) => {
-          console.log(`Player ${userName} joined game.`);
-        },
-        onPlayerMadeMove: ({ playerId, tile, placedTileColor }) => {
-          console.log("Player made move:", playerId, tile, placedTileColor);
-          addPiece(tile.y, tile.x, placedTileColor);
-        },
-        onGameHubError: (error) => {
-          console.warn("Error from game hub:", error.message);
-        },
-      });
-    }
-  }, [connection, isConnected, gameID, registerEventHandlers, addPiece]);
-
-  useEffect(() => {
-    if (winner) alert(`The winner is: ${winner}`);
-  }, [winner]);
-
-  const handleMove = async (row: number, col: number, value: string | null) => {
-    if (!connection || winner || value) return;
-
-    if (connection.state !== signalR.HubConnectionState.Connected) {
-      console.warn("Cannot make a move, SignalR connection is not connected.");
-      return;
-    }
-
-    const makeMoveMessage = {
-      gameId: gameID,
-      x: row,
-      y: col,
-    };
-
-    try {
-      await connection.invoke("MakeMove", makeMoveMessage);
-    } catch (error) {
-      console.error("Error making move:", error);
-    }
-  };
+  const { board, handleMove } = useJoinGame(gameID!);
 
   return (
     <div className="min-h-screen bg-[#161512] text-base text-[#bababa] sm:text-lg">
