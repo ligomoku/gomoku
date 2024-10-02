d7934b455d0f05034cdaceac7f4bc0b02c0bc6ae
Vitaly Kuprin <vitas_s@inbox.lv>
2024-10-02 20:52:45 +0200

    chore: responsivness
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/src/pages/JoinGame.tsx
+++ GomokuClient/packages/gomoku-core/src/pages/JoinGame.tsx
@@ -1,40 +1,44 @@
 import { useParams } from "@tanstack/react-router";
 import { Chat } from "@/features/Chat";
-import Square from "@/features/Square/Square";
 import { useJoinGame } from "@/hooks/useJoinGame";
+import MobileSquare from "@/features/Square/Mobile/MobileSquare";
+import { useMobileDesign } from "@/hooks/useMobileDesign";
 
 const JoinGame = () => {
   const { gameID } = useParams({ strict: false });
   const { board, handleMove } = useJoinGame(gameID!);
+  const isMobile = useMobileDesign();
 
   return (
     <div className="min-h-screen bg-[#161512] text-base text-[#bababa] sm:text-lg">
       <div className="font-open-sans flex flex-col items-center p-4 font-light">
         {gameID && (
-          <div className="mb-5 flex w-full flex-wrap justify-center">
-            <div className="grid-cols-19 grid">
-              {board.map((row, rowIndex) => (
-                <div className="flex" key={rowIndex}>
-                  {row.map((col, colIndex) => (
-                    <Square
-                      key={`${rowIndex}-${colIndex}`}
-                      row={rowIndex}
-                      col={colIndex}
-                      value={col}
-                      onClick={() => handleMove(rowIndex, colIndex, col)}
-                    />
-                  ))}
-                </div>
-              ))}
-            </div>
+          <>
+            <div className="mb-5 flex w-full flex-wrap justify-center">
+              <div className="grid-cols-19 grid">
+                {board.map((row, rowIndex) => (
+                  <div className="flex" key={rowIndex}>
+                    {row.map((col, colIndex) => (
+                      <MobileSquare
+                        key={`${rowIndex}-${colIndex}`}
+                        row={rowIndex}
+                        col={colIndex}
+                        value={col}
+                        onClick={() => handleMove(rowIndex, colIndex, col)}
+                        size={isMobile ? 5 : 10}
+                      />
+                    ))}
+                  </div>
+                ))}
+              </div>
 
-            <div className="ml-4 flex flex-col justify-between">
-              {/*<Timer />*/}
-              <br />
-              <br />
-              <Chat gameID={gameID} />
+              <div className="ml-4 flex flex-col justify-between">
+                <br />
+                <br />
+                <Chat gameID={gameID} />
+              </div>
             </div>
-          </div>
+          </>
         )}
       </div>
     </div>
