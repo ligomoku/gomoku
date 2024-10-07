f938cb8226c7784019e67866c5af302c9b395ca9
Vitaly Kuprin <vitas_s@inbox.lv>
2024-10-07 20:49:59 +0200

    fix(client): mobile ui and resizing for desktop view
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/src/features/Board/Board.tsx
+++ GomokuClient/packages/gomoku-core/src/features/Board/Board.tsx
@@ -1,7 +1,9 @@
 import { CellValue } from "@/hooks/useBoard";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { cva } from "class-variance-authority";
 import { useMobileDesign } from "@/hooks/useMobileDesign";
+import { ResizableBox } from "react-resizable";
+import "react-resizable/css/styles.css";
 
 export interface BoardProps {
   size: number;
@@ -23,6 +25,8 @@ const tileStyles = cva("rounded-full h-[90%] w-[90%]", {
 
 export const Board = ({ size, onTileClick, tiles }: BoardProps) => {
   const isMobile = useMobileDesign();
+  const [boardSize, setBoardSize] = useState(window.innerWidth / 2.2);
+
   const tilesElements = useMemo(
     () =>
       tiles.map((row, rowIndex) =>
@@ -50,16 +54,44 @@ export const Board = ({ size, onTileClick, tiles }: BoardProps) => {
     [tiles, onTileClick],
   );
 
-  const calculatedSize = isMobile ? 100 : 80;
+  const calculatedSize = boardSize / size;
+  const calculatedMobileSize = isMobile ? 100 : 80;
 
+  if (!isMobile) {
+    return (
+      <ResizableBox
+        width={boardSize}
+        height={boardSize}
+        onResize={(_event, { size }) => {
+          setBoardSize(size.width);
+        }}
+        resizeHandles={["se"]}
+      >
+        <div
+          className={`rounded-lg bg-[#ba8c63] shadow-md`}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <div
+            className="grid rounded-lg"
+            style={{
+              gridTemplateColumns: `repeat(${size}, ${calculatedSize}px)`,
+              gridTemplateRows: `repeat(${size}, ${calculatedSize}px)`,
+            }}
+          >
+            {tilesElements}
+          </div>
+        </div>
+      </ResizableBox>
+    );
+  }
   return (
     <div className="rounded-lg bg-[#ba8c63] p-2.5 shadow-md">
       <div
         className="grid rounded-lg"
         style={{
           //TODO: should be based on window size
-          gridTemplateColumns: `repeat(${size}, ${calculatedSize / size}vmin)`,
-          gridTemplateRows: `repeat(${size}, ${calculatedSize / size}vmin)`,
+          gridTemplateColumns: `repeat(${size}, ${calculatedMobileSize / size}vmin)`,
+          gridTemplateRows: `repeat(${size}, ${calculatedMobileSize / size}vmin)`,
         }}
       >
         {tilesElements}
