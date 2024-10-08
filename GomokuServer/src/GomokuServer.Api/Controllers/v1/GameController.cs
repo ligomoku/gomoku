47914eb63d17c5a5c159d403929a7822b2acbc73
Vitaly Kuprin <vitas_s@inbox.lv>
2024-10-08 04:42:49 +0200

    chore: temp
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>
    Signed-off-by: Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>

--- GomokuServer/src/GomokuServer.Api/Controllers/v1/GameController.cs
+++ GomokuServer/src/GomokuServer.Api/Controllers/v1/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using GomokuServer.Api.Hubs.Messages.Server;
 using GomokuServer.Api.Swagger.Examples;
 
+using Microsoft.AspNetCore.Authorization;
+
 namespace GomokuServer.Api.Controllers.v1;
 
 [ApiController]
@@ -21,16 +23,50 @@ public class GameController : Controller
 	}
 
 	/// <summary>
+	/// Get game history by game id
+	/// </summary>
+	/// <response code="200">Information about game </response>
+	/// <response code="404">Game by id not found</response>
+	[HttpGet("{gameId}/history")]
+	[ProducesResponseType(typeof(GetGameHistoryResponse), StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+	[SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(NotFoundErrorExample))]
+	public async Task<IActionResult> GetGameHistory([FromRoute] string gameId)
+	{
+		var getGameSessionResult = await _mediator.Send(new GetGameHistoryQuery { GameId = gameId });
+		return getGameSessionResult.ToApiResponse();
+	}
+
+	/// <summary>
+	/// Get all games available to join
+	/// </summary>
+	/// <response code="200">Returns list of games that are available to join</response>
+	[HttpGet]
+	[Route("/api/games/available-to-join")]
+	[ProducesResponseType(typeof(PaginatedResponse<IEnumerable<GetAvailableGamesResponse>>), StatusCodes.Status200OK)]
+	public async Task<IActionResult> GetAvailableGames([FromQuery] PaginationRequest request)
+	{
+		var query = new GetAvailableToJoinGamesQuery
+		{
+			Limit = request.Limit,
+			Offset = request.Offset
+		};
+
+		var availableGamesResult = await _mediator.Send(query);
+		return availableGamesResult.ToApiResponse();
+	}
+
+	/// <summary>
 	/// Create new game (supports both anonymous and authenticated users)
 	/// </summary>
-	/// <response code="200">Returns information about new created game</response>
+	/// <response code="200">Returns information about newly created game</response>
 	[HttpPost]
+	[AllowAnonymous] // Allow anonymous users to access this endpoint
 	[ProducesResponseType(typeof(CreateGameResponse), StatusCodes.Status200OK)]
 	[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
 	[SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestErrorExample))]
 	public async Task<IActionResult> CreateNewGame([FromBody] CreateGameRequest request)
 	{
-		// Get userId and userName from claims (nullable)
 		string? userId = User.Claims.Get(JwtClaims.UserId);
 		string? userName = User.Claims.Get(JwtClaims.UserName);
 
@@ -40,24 +76,13 @@ public class GameController : Controller
 			userName = $"Guest_{userId.Substring(0, 6)}";
 		}
 
-		var createGameResult = await _mediator.Send(new CreateGameCommand()
+		var createGameResult = await _mediator.Send(new CreateGameCommand
 		{
 			BoardSize = request.BoardSize,
 			PlayerId = userId
 		});
 
-		if (createGameResult.IsSuccess)
-		{
-			var response = new CreateGameResponse(
-				createGameResult.Value.GameId,
-				request.BoardSize
-			);
-			return Ok(response);
-		}
-		else
-		{
-			return BadRequest("Game creation failed");
-		}
+		return createGameResult.ToApiResponse();
 	}
 
 	/// <summary>
@@ -66,6 +91,7 @@ public class GameController : Controller
 	/// <response code="204">Player with specified id successfully joined the game</response>
 	/// <response code="404">Game or player with specified id not found</response>
 	[HttpPost("{gameId}/join")]
+	[AllowAnonymous] // Allow anonymous users to access this endpoint
 	[ProducesResponseType(StatusCodes.Status204NoContent)]
 	[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
 	[SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(NotFoundErrorExample))]
@@ -80,7 +106,7 @@ public class GameController : Controller
 			userName = $"Guest_{userId.Substring(0, 6)}";
 		}
 
-		var addPlayerToGameResult = await _mediator.Send(new AddPlayerToGameCommand()
+		var addPlayerToGameResult = await _mediator.Send(new AddPlayerToGameCommand
 		{
 			GameId = gameId,
 			PlayerId = userId
@@ -88,16 +114,10 @@ public class GameController : Controller
 
 		if (addPlayerToGameResult.IsSuccess)
 		{
-			var message = new PlayerJoinedGameMessage()
-			{
-				UserName = userName!
-			};
+			var message = new PlayerJoinedGameMessage { UserName = userName! };
 			await _gameHubContext.Clients.Group(gameId).SendAsync(GameHubMethod.PlayerJoinedGame, message);
-			return NoContent();
 		}
-		else
-		{
-			return NotFound("Game or player not found");
-		}
+
+		return addPlayerToGameResult.ToApiResponse();
 	}
 }
--- GomokuServer/src/GomokuServer.Application/Games/Commands/AddPlayerToGame.cs
+++ GomokuServer/src/GomokuServer.Application/Games/Commands/AddPlayerToGame.cs
@@ -1,5 +1,7 @@
 ﻿using GomokuServer.Application.Interfaces.Common;
 
+using Microsoft.Extensions.Logging;
+
 namespace GomokuServer.Application.Games.Commands;
 
 public record AddPlayerToGameCommand : ICommand
@@ -15,46 +17,64 @@ public class AddPlayerToGameCommandHandler : ICommandHandler<AddPlayerToGameComm
 {
 	private readonly IGameRepository _gameRepository;
 	private readonly IProfilesRepository _profilesRepository;
+	private readonly ILogger<AddPlayerToGameCommandHandler> _logger;
 
-	public AddPlayerToGameCommandHandler(IGameRepository gameRepository, IProfilesRepository profilesRepository)
+	public AddPlayerToGameCommandHandler(
+		IGameRepository gameRepository,
+		IProfilesRepository profilesRepository,
+		ILogger<AddPlayerToGameCommandHandler> logger)
 	{
 		_gameRepository = gameRepository;
 		_profilesRepository = profilesRepository;
+		_logger = logger;
 	}
 
 	public async Task<Result> Handle(AddPlayerToGameCommand request, CancellationToken cancellationToken)
 	{
+		_logger.LogInformation($"Attempting to add player {request.PlayerId} to game {request.GameId}");
+
+		// Check if game exists
 		var getGameResult = await _gameRepository.GetAsync(request.GameId);
 		if (getGameResult.Status == ResultStatus.NotFound)
 		{
-			return Result.NotFound();
+			_logger.LogWarning($"Game with ID {request.GameId} not found.");
+			return Result.NotFound($"Game with ID {request.GameId} not found");
 		}
 
+		// Check if player exists
 		var getProfileResult = await _profilesRepository.GetAsync(request.PlayerId);
+		if (getProfileResult.Status == ResultStatus.NotFound)
+		{
+			_logger.LogWarning($"Player with ID {request.PlayerId} not found.");
+			return Result.NotFound($"Player with ID {request.PlayerId} not found");
+		}
+
+		// Log a generic error if the result is not successful
 		if (getProfileResult.Status != ResultStatus.Ok)
 		{
-			if (getProfileResult.IsNotFound())
-			{
-				return Result.NotFound($"Player with id {request.PlayerId} not found");
-			}
-
-			return Result.Error();
+			_logger.LogError($"Error fetching player with ID {request.PlayerId}. Status: {getProfileResult.Status}");
+			return Result.Error($"Error fetching player with ID {request.PlayerId}");
 		}
 
+		// Add player to game
 		var game = getGameResult.Value;
 		var addingResult = game.AddOpponent(getProfileResult.Value);
 
 		if (!addingResult.IsValid)
 		{
+			_logger.LogWarning($"Validation failed when adding player {request.PlayerId} to game {request.GameId}: {addingResult.ValidationError}");
 			return Result.Invalid(new ValidationError(addingResult.ValidationError.ToString()));
 		}
 
+		// Save updated game
 		var saveResult = await _gameRepository.SaveAsync(game);
 		if (saveResult.Status != ResultStatus.Ok)
 		{
-			return Result.Error();
+			_logger.LogError($"Failed to save the game after adding player {request.PlayerId}.");
+			return Result.Error("Failed to save game changes");
 		}
 
+		_logger.LogInformation($"Player {request.PlayerId} successfully added to game {request.GameId}");
 		return Result.Success();
 	}
 }
