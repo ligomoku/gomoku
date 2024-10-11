34f7141b20d1f932ee75eb87b6ba294daf90edce
Vitaly Kuprin <vitas_s@inbox.lv>
2024-10-11 17:17:22 +0200

    fix(client): spectator detection
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/src/routes/game/join/$gameID.tsx
+++ GomokuClient/packages/gomoku-core/src/routes/game/join/$gameID.tsx
@@ -22,6 +22,7 @@ export const Route = createFileRoute("/game/join/$gameID")({
   },
 });
 
+//TODO: should be refracted depending on user type anonymous/authorized/spectator
 const joinGameLoader = async ({ params }: { params: { gameID: string } }) => {
   const { gameID } = params;
 
@@ -29,20 +30,31 @@ const joinGameLoader = async ({ params }: { params: { gameID: string } }) => {
     throw new Error("Game ID is required");
   }
 
-  if (sessionStorage.getItem("playerID")) {
+  const playerID = sessionStorage.getItem("playerID");
+  const fromHomeJoinedGame = sessionStorage.getItem("fromHomeJoinedGame");
+  if (playerID) {
     return;
   }
 
   const authToken = typedStorage.getItem("jwtToken");
+
   if (!authToken) {
     console.warn("No authorization token available, anonymous user detected");
+  }
+
+  if (!fromHomeJoinedGame) {
+    console.warn("Spectator detected");
+    return;
   }
 
   try {
     const response = await SwaggerServices.postApiGameByGameIdJoin({
       path: { gameId: gameID },
-      headers: getDefaultHeaders(authToken!),
+      headers: getDefaultHeaders(authToken || ""),
     });
+
+    sessionStorage.removeItem("fromHomeJoinedGame");
+
     console.log("Joined game:", response);
     return response?.data?.playerId;
   } catch (error) {
