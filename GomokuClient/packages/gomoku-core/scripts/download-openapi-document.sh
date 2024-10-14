2f16d97c9a4f44d4ae735799a4c9dbfc974a5126
Vitaly Kuprin <vitas_s@inbox.lv>
2024-10-14 02:14:58 +0200

    ci: add script and changed devops download stuff
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/scripts/download-openapi-document.sh
+++ GomokuClient/packages/gomoku-core/scripts/download-openapi-document.sh
@@ -6,6 +6,8 @@ API_VERSION="$(node -p 'require("./package.json").apiVersion')"
 
 if [ "$1" = "localhost" ]; then
   API_URL="http://localhost:62411/swagger/v${API_VERSION}/swagger.json"
+elif [ "$1" = "CI" ]; then
+  API_URL="http://localhost:8080/swagger/v${API_VERSION}/swagger.json"
 else
   API_URL="https://gomoku-gi8o.onrender.com/swagger/v${API_VERSION}/swagger.json"
 fi
