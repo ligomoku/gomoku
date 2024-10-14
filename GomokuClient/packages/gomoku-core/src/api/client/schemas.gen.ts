1c6f9de47837b63c0fb289dd5bc5b839263b7ae3
Aleksandrs Vaguscenko <54764339+Alequez97@users.noreply.github.com>
2024-10-14 18:36:57 +0300

    fix(client): last move marker (#171)
    Signed-off-by: Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>

--- GomokuClient/packages/gomoku-core/src/api/client/schemas.gen.ts
+++ GomokuClient/packages/gomoku-core/src/api/client/schemas.gen.ts
@@ -133,9 +133,6 @@ export const GetGameHistoryResponseSchema = {
         $ref: "#/components/schemas/TileDto",
       },
     },
-    lastMove: {
-      $ref: "#/components/schemas/TileDto",
-    },
   },
   additionalProperties: false,
 } as const;
--- GomokuServer/src/GomokuServer.Application/Games/Responses/GetGameHistoryResponse.cs
+++ GomokuServer/src/GomokuServer.Application/Games/Responses/GetGameHistoryResponse.cs
@@ -21,6 +21,4 @@ public class GetGameHistoryResponse
 
 	[Required]
 	public required IReadOnlyDictionary<int, TileDto> MovesHistory { get; init; }
-
-	public TileDto? LastMove { get; init; }
 }
