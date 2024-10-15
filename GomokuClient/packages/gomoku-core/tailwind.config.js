c14e7a85bd12192363d53f102a2a8cbc84d077b3
Vitaly Kuprin <vitas_s@inbox.lv>
2024-10-15 03:44:24 +0200

    chore(client): improve notification
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/tailwind.config.js
+++ GomokuClient/packages/gomoku-core/tailwind.config.js
@@ -52,7 +52,6 @@ export default {
           4: "hsl(var(--chart-4))",
           5: "hsl(var(--chart-5))",
         },
-        // Add Radix UI colors
         ...blackA,
         ...green,
         ...mauve,
