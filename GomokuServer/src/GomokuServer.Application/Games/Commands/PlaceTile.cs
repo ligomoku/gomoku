caf5d6111f584f01e43f7e4c9f345249c4ab840c
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2024-10-15 11:14:35 +0300

    fix(server): place tile logic. add tests for fixed case
    Signed-off-by: Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>

--- GomokuServer/src/GomokuServer.Application/Games/Commands/PlaceTile.cs
+++ GomokuServer/src/GomokuServer.Application/Games/Commands/PlaceTile.cs
@@ -15,19 +15,37 @@ public record PlaceTileCommand : ICommand<PlaceTileResponse>
 public class PlaceTileCommandHandler : ICommandHandler<PlaceTileCommand, PlaceTileResponse>
 {
 	private readonly IRegisteredGamesRepository _registeredGamesRepository;
+	private readonly IAnonymousGamesRepository _anonymousGamesRepository;
 
-	public PlaceTileCommandHandler(IRegisteredGamesRepository registeredGamesRepository)
+	public PlaceTileCommandHandler(
+		IRegisteredGamesRepository registeredGamesRepository,
+		IAnonymousGamesRepository anonymousGamesRepository
+	)
 	{
 		_registeredGamesRepository = registeredGamesRepository;
+		_anonymousGamesRepository = anonymousGamesRepository;
 	}
 
 	public async Task<Result<PlaceTileResponse>> Handle(PlaceTileCommand request, CancellationToken cancellationToken)
 	{
-		var getGameResult = await _registeredGamesRepository.GetAsync(request.GameId);
+		var getRegisteredGameResult = await _registeredGamesRepository.GetAsync(request.GameId);
+
+		if (getRegisteredGameResult.IsSuccess)
+		{
+			return await TryPlaceTile(_registeredGamesRepository, getRegisteredGameResult, request);
+		}
+
+		var getAnonymousGameResult = await _anonymousGamesRepository.GetAsync(request.GameId);
+		return await TryPlaceTile(_anonymousGamesRepository, getAnonymousGameResult, request);
+	}
+
+	private async Task<Result<PlaceTileResponse>> TryPlaceTile(IGamesRepository gamesRepository, Result<Game> getGameResult, PlaceTileCommand request)
+	{
 		if (getGameResult.Status == ResultStatus.NotFound)
 		{
 			return Result.NotFound();
 		}
+
 		var game = getGameResult.Value;
 		var tileDto = request.Tile;
 		var tilePlacementResult = game.PlaceTile(new Tile(tileDto.X, tileDto.Y), request.PlayerId);
@@ -37,7 +55,7 @@ public class PlaceTileCommandHandler : ICommandHandler<PlaceTileCommand, PlaceTi
 			return Result.Invalid(new ValidationError(tilePlacementResult.ValidationError.ToString()));
 		}
 
-		var saveResult = await _registeredGamesRepository.SaveAsync(game);
+		var saveResult = await gamesRepository.SaveAsync(game);
 		return saveResult.Map(_ =>
 		{
 			var winningSequence = tilePlacementResult.WinningSequence?.Select(tile => new TileDto(tile.X, tile.Y)).ToList();
--- GomokuServer/tests/GomokuServer.Application.FunctionalTests/Tests/Games/Commands/PlaceTileTests.cs
+++ GomokuServer/tests/GomokuServer.Application.FunctionalTests/Tests/Games/Commands/PlaceTileTests.cs
@@ -8,11 +8,10 @@ namespace GomokuServer.Application.FunctionalTests.Tests.Games.Commands;
 public class PlaceTileTests : FunctionalTestBase
 {
 	[Test]
-	public async Task PlaceTile_PassExistingGameAndPlayerId_ShouldSuccessfullyPlaceTileInRegisteredGame()
+	public async Task PlaceTile_PassExistingPlayerIdAndRegisteredGameId_ShouldSuccessfullyPlaceTileInRegisteredGame()
 	{
 		// Arrange
 		var game = _testDataProvider.GetGame_TwoPlayersJoined_NoMoves();
-		var blackPlayer = game.Players.Black;
 		await RegisteredGamesRepository.SaveAsync(game);
 
 		// Act
@@ -25,5 +24,36 @@ public class PlaceTileTests : FunctionalTestBase
 
 		// Assert
 		placeTileResult.Status.Should().Be(ResultStatus.Ok);
+
+		var getUpdatedGameResult = await RegisteredGamesRepository.GetAsync(game.GameId);
+		getUpdatedGameResult.Value.MovesHistory.Count.Should().Be(1);
+
+		var getAnonymousGameResult = await AnonymousGamesRepository.GetAsync(game.GameId);
+		getAnonymousGameResult.Status.Should().Be(ResultStatus.NotFound);
+	}
+
+	[Test]
+	public async Task PlaceTile_PassExistingPlayerIdAndAnonymousGameId_ShouldSuccessfullyPlaceTileInAnonymousGame()
+	{
+		// Arrange
+		var game = _testDataProvider.GetGame_TwoPlayersJoined_NoMoves();
+		await AnonymousGamesRepository.SaveAsync(game);
+
+		// Act
+		var placeTileResult = await SendAsync(new PlaceTileCommand
+		{
+			GameId = game.GameId,
+			PlayerId = game.Players.Black!.Id,
+			Tile = new TileDto(0, 0)
+		});
+
+		// Assert
+		placeTileResult.Status.Should().Be(ResultStatus.Ok);
+
+		var getUpdatedGameResult = await AnonymousGamesRepository.GetAsync(game.GameId);
+		getUpdatedGameResult.Value.MovesHistory.Count.Should().Be(1);
+
+		var getRegisteredGameResult = await RegisteredGamesRepository.GetAsync(game.GameId);
+		getRegisteredGameResult.Status.Should().Be(ResultStatus.NotFound);
 	}
 }
