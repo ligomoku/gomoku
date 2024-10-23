1718b053bc8e9e950d10f8ae43fed36f06decb68
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2024-10-23 23:08:45 +0300

    fix(client): format
    Signed-off-by: Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/src/features/GameTime/GameTime.tsx
+++ GomokuClient/packages/gomoku-core/src/features/GameTime/GameTime.tsx
@@ -42,15 +42,15 @@ export const GameTime = ({
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
 
-    const minutesString = minutes.toString().padStart(2, '0');
-    const secondsString = remainingSeconds.toString().padStart(2, '0');
+    const minutesString = minutes.toString().padStart(2, "0");
+    const secondsString = remainingSeconds.toString().padStart(2, "0");
 
     return `${minutesString}:${secondsString}`;
-  }
+  };
 
   useEffect(() => {
     const timerId = setTimeout(() => {
-      setSecondsLeft(prev => prev - 1);
+      setSecondsLeft((prev) => prev - 1);
     }, 1000);
 
     return () => clearInterval(timerId);
