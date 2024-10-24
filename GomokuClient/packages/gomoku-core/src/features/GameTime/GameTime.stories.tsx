8d31a429355bf29eba2432a1ec0cc9457c49bd13
Vitaly Kuprin <vitas_s@inbox.lv>
2024-10-24 03:38:14 +0200

    fix: story typecheck
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/src/features/GameTime/GameTime.stories.tsx
+++ GomokuClient/packages/gomoku-core/src/features/GameTime/GameTime.stories.tsx
@@ -13,35 +13,33 @@ export default {
 export const Default = () => (
   <GameTime
     moves={[]}
-    initialTimeInSeconds={1000}
-    currentPlayer="black"
+    activePlayer="black"
     players={[
       { name: "Player 1", color: "#7cb342" },
       { name: "Player 2", color: "#b0b0b0" },
     ]}
-    clockTime="01:00"
-    onAddMove={() => alert("Add move clicked")}
     onUndo={() => alert("Undo clicked")}
     onSkip={(direction) => alert(`Skip ${direction} clicked`)}
     onFlag={() => alert("Flag clicked")}
     onReset={() => alert("Reset clicked")}
+    blackTimeLeft={1000}
+    whiteTimeLeft={1000}
   />
 );
 
 export const WithMoves = () => (
   <GameTime
-    initialTimeInSeconds={1000}
     moves={["x1 - y2", "x3 - y4", "x5 - y6", "x7 - y8", "x9 - y10"]}
-    currentPlayer="black"
+    activePlayer="black"
     players={[
       { name: "Player 1", color: "#7cb342" },
       { name: "Player 2", color: "#b0b0b0" },
     ]}
-    clockTime="01:00"
-    onAddMove={() => notification.show("Add move clicked")}
     onUndo={() => notification.show("Undo clicked")}
     onSkip={(direction) => notification.show(`Skip ${direction} clicked`)}
     onFlag={() => notification.show("Flag clicked")}
     onReset={() => notification.show("Reset clicked")}
+    blackTimeLeft={1000}
+    whiteTimeLeft={1000}
   />
 );
