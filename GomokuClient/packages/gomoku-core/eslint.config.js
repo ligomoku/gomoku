03aa3058fd78e4a1f88ecfc6a1538e034715a895
Vitaly Kuprin <vitas_s@inbox.lv>
2024-11-11 06:01:36 +0100

    chore(client): enhance linting system
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/eslint.config.js
+++ GomokuClient/packages/gomoku-core/eslint.config.js
@@ -3,7 +3,5 @@ import gomokuEslintConfig from "@gomoku/eslint-config-custom";
 
 export default tseslint.config({
   extends: [...gomokuEslintConfig],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-  },
+  ignores: ["dist, **/*.gen.ts"],
 });
