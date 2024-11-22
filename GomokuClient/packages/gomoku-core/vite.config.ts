25da4fdfdc1a9091d6fd410d2c8465d598ecd386
Vitaly Kuprin <vitas_s@inbox.lv>
2024-11-22 16:36:04 +0100

    chore: fix seqaunce
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/vite.config.ts
+++ GomokuClient/packages/gomoku-core/vite.config.ts
@@ -1,43 +1,33 @@
 // @ts-ignore
 import path from "path";
 
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+// @ts-ignore
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
 export default ({ mode }: { mode: string }) => {
-  const envDirPath = path.resolve(process.cwd(), "../../../envs");
-  const srcPath = path.resolve(__dirname, "src");
-  const env = loadEnv(mode, envDirPath);
-
-  console.debug("Environment Directory Path:", envDirPath);
-  console.debug("Source Path:", srcPath);
-  console.debug("Env:", env);
+  const envDirPath = path.resolve(__dirname, "../../../envs");
+  const srcPath = path.resolve(__dirname, "./src");
+  const env = loadEnv(mode, path.resolve(__dirname, envDirPath));
 
   return defineConfig({
     plugins: [
+      TanStackRouterVite(),
       viteReact({
         babel: {
           plugins: ["macros"],
         },
       }),
+      sentryVitePlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: "gomoku",
+        project: "javascript-react",
+      }),
     ],
     build: {
       sourcemap: true,
-      lib: {
-        entry: path.resolve(__dirname, "src/index.ts"),
-        name: "GomokuStory",
-        fileName: (format) => `gomoku-story.${format}.js`,
-        formats: ["es", "cjs"],
-      },
-      rollupOptions: {
-        external: ["react", "react-dom"],
-        output: {
-          globals: {
-            react: "React",
-            "react-dom": "ReactDOM",
-          },
-        },
-      },
     },
     envDir: envDirPath,
     resolve: {
@@ -46,7 +36,7 @@ export default ({ mode }: { mode: string }) => {
       },
     },
     server: {
-      port: parseInt(env.VITE_LOCALHOST_PORT) || 3000,
+      port: parseInt(env.VITE_LOCALHOST_PORT),
     },
   });
 };
