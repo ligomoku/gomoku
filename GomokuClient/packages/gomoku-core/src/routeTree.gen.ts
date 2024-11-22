9ed28900723f51eb9ceffd32e138a69d69708d46
Vitaly Kuprin <vitas_s@inbox.lv>
2024-11-22 16:51:19 +0100

    chore: route gen
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/src/routeTree.gen.ts
+++ GomokuClient/packages/gomoku-core/src/routeTree.gen.ts
@@ -8,130 +8,111 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from '@tanstack/react-router'
 
 // Import Routes
 
-import { Route as rootRoute } from "./routes/__root";
-import { Route as GameJoinGameIDImport } from "./routes/game/join/$gameID";
+import { Route as rootRoute } from './routes/__root'
+import { Route as GameJoinGameIDImport } from './routes/game/join/$gameID'
 
 // Create Virtual Routes
 
-const TestLazyImport = createFileRoute("/test")();
-const ProfileLazyImport = createFileRoute("/profile")();
-const IndexLazyImport = createFileRoute("/")();
+const ProfileLazyImport = createFileRoute('/profile')()
+const IndexLazyImport = createFileRoute('/')()
 
 // Create/Update Routes
 
-const TestLazyRoute = TestLazyImport.update({
-  id: "/test",
-  path: "/test",
-  getParentRoute: () => rootRoute,
-} as any).lazy(() => import("./routes/test.lazy").then((d) => d.Route));
-
 const ProfileLazyRoute = ProfileLazyImport.update({
-  id: "/profile",
-  path: "/profile",
+  id: '/profile',
+  path: '/profile',
   getParentRoute: () => rootRoute,
-} as any).lazy(() => import("./routes/profile.lazy").then((d) => d.Route));
+} as any).lazy(() => import('./routes/profile.lazy').then((d) => d.Route))
 
 const IndexLazyRoute = IndexLazyImport.update({
-  id: "/",
-  path: "/",
+  id: '/',
+  path: '/',
   getParentRoute: () => rootRoute,
-} as any).lazy(() => import("./routes/index.lazy").then((d) => d.Route));
+} as any).lazy(() => import('./routes/index.lazy').then((d) => d.Route))
 
 const GameJoinGameIDRoute = GameJoinGameIDImport.update({
-  id: "/game/join/$gameID",
-  path: "/game/join/$gameID",
+  id: '/game/join/$gameID',
+  path: '/game/join/$gameID',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 // Populate the FileRoutesByPath interface
 
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    "/": {
-      id: "/";
-      path: "/";
-      fullPath: "/";
-      preLoaderRoute: typeof IndexLazyImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/profile": {
-      id: "/profile";
-      path: "/profile";
-      fullPath: "/profile";
-      preLoaderRoute: typeof ProfileLazyImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/test": {
-      id: "/test";
-      path: "/test";
-      fullPath: "/test";
-      preLoaderRoute: typeof TestLazyImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/game/join/$gameID": {
-      id: "/game/join/$gameID";
-      path: "/game/join/$gameID";
-      fullPath: "/game/join/$gameID";
-      preLoaderRoute: typeof GameJoinGameIDImport;
-      parentRoute: typeof rootRoute;
-    };
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexLazyImport
+      parentRoute: typeof rootRoute
+    }
+    '/profile': {
+      id: '/profile'
+      path: '/profile'
+      fullPath: '/profile'
+      preLoaderRoute: typeof ProfileLazyImport
+      parentRoute: typeof rootRoute
+    }
+    '/game/join/$gameID': {
+      id: '/game/join/$gameID'
+      path: '/game/join/$gameID'
+      fullPath: '/game/join/$gameID'
+      preLoaderRoute: typeof GameJoinGameIDImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
 // Create and export the route tree
 
 export interface FileRoutesByFullPath {
-  "/": typeof IndexLazyRoute;
-  "/profile": typeof ProfileLazyRoute;
-  "/test": typeof TestLazyRoute;
-  "/game/join/$gameID": typeof GameJoinGameIDRoute;
+  '/': typeof IndexLazyRoute
+  '/profile': typeof ProfileLazyRoute
+  '/game/join/$gameID': typeof GameJoinGameIDRoute
 }
 
 export interface FileRoutesByTo {
-  "/": typeof IndexLazyRoute;
-  "/profile": typeof ProfileLazyRoute;
-  "/test": typeof TestLazyRoute;
-  "/game/join/$gameID": typeof GameJoinGameIDRoute;
+  '/': typeof IndexLazyRoute
+  '/profile': typeof ProfileLazyRoute
+  '/game/join/$gameID': typeof GameJoinGameIDRoute
 }
 
 export interface FileRoutesById {
-  __root__: typeof rootRoute;
-  "/": typeof IndexLazyRoute;
-  "/profile": typeof ProfileLazyRoute;
-  "/test": typeof TestLazyRoute;
-  "/game/join/$gameID": typeof GameJoinGameIDRoute;
+  __root__: typeof rootRoute
+  '/': typeof IndexLazyRoute
+  '/profile': typeof ProfileLazyRoute
+  '/game/join/$gameID': typeof GameJoinGameIDRoute
 }
 
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath;
-  fullPaths: "/" | "/profile" | "/test" | "/game/join/$gameID";
-  fileRoutesByTo: FileRoutesByTo;
-  to: "/" | "/profile" | "/test" | "/game/join/$gameID";
-  id: "__root__" | "/" | "/profile" | "/test" | "/game/join/$gameID";
-  fileRoutesById: FileRoutesById;
+  fileRoutesByFullPath: FileRoutesByFullPath
+  fullPaths: '/' | '/profile' | '/game/join/$gameID'
+  fileRoutesByTo: FileRoutesByTo
+  to: '/' | '/profile' | '/game/join/$gameID'
+  id: '__root__' | '/' | '/profile' | '/game/join/$gameID'
+  fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
-  IndexLazyRoute: typeof IndexLazyRoute;
-  ProfileLazyRoute: typeof ProfileLazyRoute;
-  TestLazyRoute: typeof TestLazyRoute;
-  GameJoinGameIDRoute: typeof GameJoinGameIDRoute;
+  IndexLazyRoute: typeof IndexLazyRoute
+  ProfileLazyRoute: typeof ProfileLazyRoute
+  GameJoinGameIDRoute: typeof GameJoinGameIDRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexLazyRoute: IndexLazyRoute,
   ProfileLazyRoute: ProfileLazyRoute,
-  TestLazyRoute: TestLazyRoute,
   GameJoinGameIDRoute: GameJoinGameIDRoute,
-};
+}
 
 export const routeTree = rootRoute
   ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>();
+  ._addFileTypes<FileRouteTypes>()
 
 /* ROUTE_MANIFEST_START
 {
@@ -141,7 +122,6 @@ export const routeTree = rootRoute
       "children": [
         "/",
         "/profile",
-        "/test",
         "/game/join/$gameID"
       ]
     },
@@ -150,9 +130,6 @@ export const routeTree = rootRoute
     },
     "/profile": {
       "filePath": "profile.lazy.tsx"
-    },
-    "/test": {
-      "filePath": "test.lazy.tsx"
     },
     "/game/join/$gameID": {
       "filePath": "game/join/$gameID.tsx"
