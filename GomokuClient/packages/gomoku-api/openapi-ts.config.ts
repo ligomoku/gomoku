382840f56a9dbae1b289924952433a15f22a2463
Vitaly Kuprin <vitas_s@inbox.lv>
2024-11-22 04:55:49 +0100

    chore: lockfiels
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-api/openapi-ts.config.ts
+++ GomokuClient/packages/gomoku-api/openapi-ts.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
   client: "@hey-api/client-fetch",
-  input: "src/api/schema.json",
+  input: "schema.json",
   output: {
     format: "prettier",
     lint: "eslint",
-    path: "src/api/client",
+    path: "client",
   },
   plugins: ["@tanstack/react-query"],
   // @ts-ignore
