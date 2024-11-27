1c6ba5f31f6cecabd810f173225c8c69dd613676
Vitaly Kuprin <vitas_s@inbox.lv>
2024-11-28 00:01:41 +0100

    chore: test itnegration
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/src/pages/Test.tsx
+++ GomokuClient/packages/gomoku-core/src/pages/Test.tsx
@@ -1,6 +1,21 @@
+import { SwaggerServices } from "@gomoku/api";
 import { Board } from "@gomoku/story";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+
+import type { SwaggerTypes } from "@gomoku/api";
+
+import { Headers } from "@/utils";
 
 export const Test = () => {
+
+  const { data, error } = useRapfiEngine();
+
+  useEffect(() => {
+    console.log(data);
+    console.error(error);
+  }, [data, error]);
+
   return (
     <Board
       size={19}
@@ -9,3 +24,24 @@ export const Test = () => {
     />
   );
 };
+
+
+const useRapfiEngine = () =>
+    useQuery<
+      SwaggerTypes.GetApiV1RapfiTestResponse,
+      SwaggerTypes.GetApiV1RapfiTestError
+    >({
+      queryKey: ["rapfiEngineTest", null],
+      queryFn: async () => {
+        const response = await SwaggerServices.getApiV1RapfiTest({
+          headers: Headers.getDefaultHeaders(),
+        });
+
+        if (!response.data) {
+          throw new Error("Invalid game data received");
+        }
+
+        return response.data;
+      },
+      refetchInterval: 5000,
+    });
