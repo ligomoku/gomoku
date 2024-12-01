8e39d5f9a3fe0704202c884a16800c4dac5b5fcb
Vitaly Kuprin <vitas_s@inbox.lv>
2024-12-01 01:20:39 +0100

    chore(client): rewrite profile page
    Signed-off-by: Vitaly Kuprin <vitas_s@inbox.lv>

--- GomokuClient/packages/gomoku-core/src/pages/Profile.tsx
+++ GomokuClient/packages/gomoku-core/src/pages/Profile.tsx
@@ -1,15 +1,12 @@
 import {
   getApiProfilesUsernameGamesQueryKey,
-  SwaggerServices,
   useGetApiProfilesUsernameGames,
 } from "@gomoku/api";
-import { t } from "@lingui/macro";
+import { t } from "@lingui/core/macro";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { Activity, ChevronUp, Users } from "lucide-react";
 import { useRef, useCallback } from "react";
 import { LineChart, Line, XAxis, YAxis, ResponsiveContainer } from "recharts";
-
-import type { SwaggerTypes } from "@gomoku/api";
 
 import { useAuthToken } from "@/context";
 import { Headers } from "@/utils";
