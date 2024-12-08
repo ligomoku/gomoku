b7d1507ccbdf8e67207a25ad1c2bca0a65a61a72
Aleksandrs Vaguscenko <54764339+Alequez97@users.noreply.github.com>
2024-12-08 19:31:53 +0200

    fix(server): fix open api validation errors (#285)
    * delete hubs from open api spec
    
    Signed-off-by: Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
    
    * fix(client): codegen
    
    Signed-off-by: Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
    
    * fix(server): remove obsolete nunit verifier in analyzer tests project
    
    Signed-off-by: Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
    
    ---------
    
    Signed-off-by: Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>

--- GomokuServer/tests/GomokuServer.Analyzer.UnitTests/AnalyzerTestBase.cs
+++ GomokuServer/tests/GomokuServer.Analyzer.UnitTests/AnalyzerTestBase.cs
@@ -1,15 +1,14 @@
 ﻿using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Testing;
-using Microsoft.CodeAnalysis.Testing.Verifiers;
 
 namespace GomokuServer.Analyzer.UnitTests;
 
 public abstract class AnalyzerTestBase<TAnalyzer> where TAnalyzer : DiagnosticAnalyzer, new()
 {
-	protected CSharpAnalyzerTest<TAnalyzer, NUnitVerifier> CreateAnalyzerTest(string sourceCode, params DiagnosticResult[] expectedDiagnostics)
+	protected CSharpAnalyzerTest<TAnalyzer, DefaultVerifier> CreateAnalyzerTest(string sourceCode, params DiagnosticResult[] expectedDiagnostics)
 	{
-		var test = new CSharpAnalyzerTest<TAnalyzer, NUnitVerifier>
+		var test = new CSharpAnalyzerTest<TAnalyzer, DefaultVerifier>
 		{
 			TestCode = sourceCode
 		};
